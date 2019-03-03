03a70fbd6f16983f7d7fbc2fe7044ce5972cbf28
Errol <errol.lin@rdgroup.com.tw>
2019-03-04 00:00:47 +0800

    add: parseApi

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,45 @@
 import express from "express";
+import axios from "axios";
+import cheerio from "cheerio";
+
 const app = express();
 const port = 3000;
 
-app.get("/", (req, res) => res.send("Hello World!"));
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+  crawler();
+});
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+
+function crawler() {
+  axios
+    .get("http://api.winner7788u.com/doc/apiLearnPage_user.php")
+    .then(function(response) {
+      // handle success
+      if (response.status === 200) {
+        parseApi(response.data);
+      }
+    })
+    .catch(function(error) {
+      // handle error
+      console.log(error);
+    })
+    .then(function() {
+      // always executed
+    });
+}
+
+function parseApi(responseData: any): void {
+  const $ = cheerio.load(responseData);
+  const Apis = $("table.table tr td");
+  Apis.each((index, api) => {
+    const parseApi = cheerio.load(api);
+    const title = parseApi("div div b").text();
+    console.log(title);
+    const url = parseApi("div div")
+      .first()
+      .text();
+    console.log(url);
+  });
+}
